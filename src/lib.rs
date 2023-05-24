712d735fac898eb537ae5134318804d9951d21c2
Wladimir Palant <fqcgithub@palant.de>
2023-05-24 13:31:59 +0200

    Fixed unused import in unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -54,7 +54,6 @@ pub fn find_smali_files(dir: &PathBuf) -> Result<Vec<SmaliClass>, SmaliError>
 
 #[cfg(test)]
 mod tests {
-    use std::fs;
     use std::path::Path;
     use crate::types::{MethodSignature, ObjectIdentifier, SmaliClass, TypeSignature};
 
